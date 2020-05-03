e382f359912882db34d04e5a456a790012c4a5ae
f-sanges <63789710+f-sanges@users.noreply.github.com>
2020-05-04 00:30:42 +0200

    Kafka project

---
+++ Kafka/src/main/java/Producer.java
@@ -0,0 +1,61 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Date;
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+
+        String IP_PORT_Bootstrap_Servers = "192.168.252.100:9092, 192.168.252.100:9093, 192.168.252.100:9094";
+        String Topic_Name = "numbers";
+
+        String client_id = "My-Producer";
+
+        // REF: https://kafka.apache.org/25/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
+        Properties props = new Properties();
+        props.put("bootstrap.servers", IP_PORT_Bootstrap_Servers);
+        //props.put("acks", "all");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+
+        int Number_of_Records = 10;
+
+        // EXAMPLE 1 Messages, Keys and Values as a String
+//        try{
+//            for (int i = 0; i < Number_of_Records; i++) {
+//                String message = String.format("Producer %s has sent %s message at %s", client_id, i, new Date());
+//
+//                producer.send(new ProducerRecord<String, String>(Topic_Name, Integer.toString(i), Integer.toString(i)));
+//            }
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        } finally {
+//            producer.close();
+//        }
+
+
+        //EXAMPLE 2: Formatted string as message and messages are sent with 300ms delay (3 messages / second)
+        try {
+            for (int i = 0; i < Number_of_Records; i++) {
+                String message = String.format("Producer %s has sent message %s at %s", client_id, i, new Date());
+                System.out.println(message);
+                producer.send(new ProducerRecord<>(Topic_Name, Integer.toString(i), message));
+                Thread.sleep(300);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+
+
+
+
+
+    }
+}
